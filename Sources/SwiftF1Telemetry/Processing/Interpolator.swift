ab6f971f3050b152519033cdd3b85baef973f959
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-04-14 23:40:32 +0200

    Remove Interpolator struct from the telemetry processing module, simplifying the codebase by eliminating unused functionality.

--- Sources/SwiftF1Telemetry/Processing/Interpolator.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Interpolator {
-    func interpolate(samples: [TelemetrySample]) -> [TelemetrySample] {
-        // TODO: Add proper resampling onto a regular lap timeline when upstream data is available.
-        samples.sorted { $0.sessionTime < $1.sessionTime }
-    }
-}
