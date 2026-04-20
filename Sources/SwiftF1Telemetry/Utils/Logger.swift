d9f9b69badd7c2a8b1507d4f94ca8e398a633033
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-04-21 00:00:51 +0200

    Refactor Logger to write debug messages to standard error using FileHandle

--- Sources/SwiftF1Telemetry/Utils/Logger.swift
+++ Sources/SwiftF1Telemetry/Utils/Logger.swift
@@ -3,7 +3,9 @@ import Foundation
 enum Logger {
     static func debug(_ message: @autoclosure () -> String) {
         #if DEBUG
-        fputs("[SwiftF1Telemetry] \(message())\n", stderr)
+        let line = "[SwiftF1Telemetry] \(message())\n"
+        guard let data = line.data(using: .utf8) else { return }
+        FileHandle.standardError.write(data)
         #endif
     }
 }
