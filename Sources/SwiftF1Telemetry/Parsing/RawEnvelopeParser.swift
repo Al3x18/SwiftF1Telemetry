89632dbfe0329d6e923c0f43eaef97e59981081a
Alex De Pasquale <alexdepasquale1997@gmail.com>
2026-04-27 18:50:33 +0200

    Refactor RawEnvelopeParser.swift to remove conditional imports for zlib, simplifying the code structure.

--- Sources/SwiftF1Telemetry/Parsing/RawEnvelopeParser.swift
+++ Sources/SwiftF1Telemetry/Parsing/RawEnvelopeParser.swift
@@ -1,11 +1,5 @@
 import Foundation
-#if canImport(zlib)
-import zlib
-#elseif canImport(CZlib)
 import CZlib
-#else
-#error("SwiftF1Telemetry requires a zlib module (zlib or CZlib) to decode compressed telemetry streams.")
-#endif
 
 struct RawJSONStreamLine {
     let sessionTime: TimeInterval
